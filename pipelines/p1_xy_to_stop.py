3ac1ac260b3c41be4b4919cd072e8f5d1c30b26c
pkelaita <pierce@kelaita.com>
2021-02-13 14:11:19 -0800

    added 1nn to p1

--- pipelines/p1_xy_to_stop.py
+++ pipelines/p1_xy_to_stop.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+import sklearn.neighbors as knn
 import os
 
 from utils import constants
+
+
+# Parameters
+KNN_LEAF_SIZE = 50
 
 
 def _print_unique(df):
@@ -13,7 +18,10 @@ def _print_unique(df):
         print()
 
 
-def load_inputs():
+def _load_inputs():
+    """
+    Load orca->xy and xy->stops datasets, filter for values we care about.
+    """
 
     # Load data
     path1 = os.path.join(constants.DATA_DIR, 'orca_by_xy.csv')
@@ -24,6 +32,7 @@ def load_inputs():
     # Clean and filter
     orca_df = orca_df[orca_df['boarding_longitude'].notnull()]
     orca_df = orca_df[orca_df['boarding_latitude'].notnull()]
+
     orca_df = orca_df[orca_df['boarding_type'] != 'Transfer']
     orca_df = orca_df[orca_df['mode_abbrev'] == 'Bus']
     orca_df = orca_df[orca_df['agency'] == 'KCM']
@@ -35,37 +44,68 @@ def load_inputs():
     return orca_df, stop_df
 
 
-def get_1nn(orca_df, stop_df, orca_idx):
-    o_row = orca_df.iloc[orca_idx]
-    o_lat = o_row['boarding_latitude']
-    o_lon = o_row['boarding_longitude']
+def _reduce_stop_df(stop_df):
+    """
+    Aggregate stop_df to only include unique stop IDs by averaging the xy
+    coordinates of duplicates.
+    """
 
-    stop_idx_nn = 0
-    dist_nn = np.inf
+    return stop_df
 
-    def l1_norm(x1, y1, x2, y2):
-        return np.abs(np.linalg.norm(np.array([x1, y1]) - np.array([x2, y2])))
 
-    for stop_idx in range(0, stop_df.shape[0]):
-        s_row = stop_df.iloc[stop_idx]
-        s_lat = s_row['lat']
-        s_lon = s_row['lon']
+def _map_nearest_neighbors(orca_df, stop_df):
+    """
+    Find the stop ID and route number for each entry in orca_df by the nearest
+    xy pair in stop_df.
+    """
 
-        dist = l1_norm(o_lat, o_lon, s_lat, s_lon)
-        if dist < dist_nn:
-            dist_nn = dist
-            stop_idx_nn = stop_idx
+    # Get knn tree
+    def get_pair(s_row):
+        return (float(s_row['lat']), float(s_row['lon']))
 
-    print(stop_idx_nn)
-    print(dist_nn)
-    print(stop_df.iloc[stop_idx_nn])
-    return stop_idx_nn
+    x = [get_pair(stop_df.iloc[i]) for i in range(stop_df.shape[0])]
+    tree = knn.KDTree(x, leaf_size=KNN_LEAF_SIZE)
+
+    # Run 1nn over dataset
+    n = orca_df.shape[0]
+    for i in range(n):
+
+        # TODO implement reference table for duplicate xy pairs
+
+        # Query knn tree
+        o_row = orca_df.iloc[i]
+        o_lat = o_row['boarding_latitude']
+        o_lon = o_row['boarding_longitude']
+        x_i = np.array([o_lat, o_lon]).reshape((1, -1))
+
+        dist, nn_index = tree.query(x_i, k=1)
+        nn_index = nn_index[0]  # unpack single value
+
+        s_row = stop_df.iloc[nn_index]
+        s_lat = float(s_row['lat'])
+        s_lon = float(s_row['lon'])
+
+        if i % (n / 100) == 0:
+            print(
+                f'{i} / {orca_df.shape[0]}\t'
+                f'({round(100 * i / n, 2)}%)'
+            )
 
 
 def run_pipeline():
-    orca_df, stop_df = load_inputs()
+    """
+    Creates a mapping from each entry in winter 2019 ORCA dataset to its
+    associated stop ID and route number.
+    """
 
-    # get_1nn(orca_df, stop_df, 30)
+    orca_df, stop_df = _load_inputs()
+    print('Done loading')
+
+    # stop_df = _reduce_stop_df(stop_df)
+    # print('Done reducing')
+
+    # _map_nearest_neighbors(orca_df, stop_df)
+    # print('Done mapping')
 
     print('ORCA_DF')
     _print_unique(orca_df)
